24b2b53f14328c8b073bc4a44ac87a7b3eaa206c
lbd0 <dlqkek8@naver.com>
2026-03-23 11:35:38 +0900

    [Add] upload project files

---
+++ Source/dekideki_v_5_6/dekideki_v_5_6.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "dekideki_v_5_6.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, dekideki_v_5_6, "dekideki_v_5_6" );
